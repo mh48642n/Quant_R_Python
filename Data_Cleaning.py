725336d9a5c5b380f47defa2e1767152e6f3c156
mh48642n <mh48642n@pace.edu>
2023-04-20 19:28:16 -0400

    Not many changes

--- Data_Cleaning.py
+++ Data_Cleaning.py
@@ -5,14 +5,15 @@ Created on Wed Apr 19 14:22:18 2023
 @author: marvi
 """
 
-
-from directory_stuff import * 
 import pandas as pd
 
-'This makes sure the directory is only this'
-stuff()
+'This makes sure the directory is only this//not working right now :('
 
 'So here I will clean the data by grouping the comparables and all their columns'
 'I will also attempt to try to group the data by neighborhood'
 
 'First call the excel file using the pandas library'
+data = pd.read_excel("C:\\Users\\marvi\\OneDrive\\Documents\\GitHub\\Quant_R_Python\\Cleaned_Condo_Data.xlsx")
+print(data)
+
+'Make a function that wrangles the columns that have numeric values and groups or aggregates by neighborhood'

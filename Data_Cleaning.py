b4ba0b2c84392665af3464e54665530be0758ea7
mh48642n <mh48642n@pace.edu>
2023-04-30 23:57:16 -0400

    Cleaned Data

--- Data_Cleaning.py
+++ Data_Cleaning.py
@@ -6,11 +6,8 @@ Created on Wed Apr 19 14:22:18 2023
 """
 
 import pandas as pd
-import numpy as np
 import re
 
-
-'This makes sure the directory is only this//not working right now :('
 
 'So here I will clean the data by grouping the comparables and all their columns'
 'I will also attempt to try to group the data by neighborhood'
@@ -18,9 +15,8 @@ import re
 'First call the excel file using the pandas library'
 data = "C:\\Users\\marvi\\OneDrive\\Documents\\GitHub\\Quant_R_Python\\Cleaned_Condo_Data.xlsx"
 data = pd.read_excel(data)
+data1 = data
 
-'This prints out the datatypes for all columns in the object called data'
-print(data.dtypes)
 
 'So the output of this function should be the dataframe with a key that corresponds to columns' 
 'with values of type = float64. Also it will account for certain columns that are int64'
@@ -43,97 +39,78 @@ def filter_out(data):
         else:
             continue
     return type_dict
-def regextolist(columns): 
+def regextolist(data, columns): 
    'First we define the pattern we are looking for with exp that will be taken as an input'
-   'Works'
-   r0 = re.compile("Gross SqFt.\d")
-   gsqft = list(filter(r0.match, columns))
-   print("Gross Sqft:", gsqft)
-   
-   'Works'
-   r1 = re.compile(".*Expense.\d")
-   'r1 = re.compile(input(".*Expense.\d"))'
-   expenselist = list(filter(r1.match, columns))
-   print("Estimated Expense:" ,expenselist)
-   
-   'Works'
-   r2 = re.compile("Expense.*\d")
-   epsqft = list(filter(r2.match, columns))
-   print("Expense per Sqft:",epsqft)
-   
-   'Works'
-   r3 = re.compile(".*Gross Income.\d")
-   egross = list(filter(r3.match, columns))
-   print("Estimated Gross Incomes:", egross)
-   
-   'Works'
-   r4 = re.compile("(\AN.*.\d)")
-   netop = list(filter(r4.match, columns))
-   print("Net: ",netop)
-   
-   'Works'
-   r5 = re.compile("Total Units.\d")
-   tu1 = list(filter(r5.match, columns))
-   print("TU", tu1)
-   
-   'Works'
-   r6 = re.compile(".*Income per SqFt.\d")
-   gips = list(filter(r6.match, columns))
-   print("Gross Income per SqFt:", gips)
-   
-   'Works'
-   r7 = re.compile(".*Value.\d")
-   fmv = list(filter(r7.match, columns))
-   print("Full Market Value:", fmv)
-   
-   'Works'
-   r8 = re.compile("^M.*.*.*\d$")
-   mvps = list(filter(r8.match, columns))
-   print("Market Value per Sqft:", mvps)
-   
-   'Now I will put this in a while loop and iterate through each using the phrases discovered for the patterns'
-   'Make use of split-apply-merge methodology'
-   'Take out the rows that I need, make it a dataframe, apply aggregate function(mean), join the new column back'
-   """Patterns:
+     
+   """ Patterns:
        Total Units:             Total Units.\d
        Gross Sqft:              Gross SqFt.\d
        Estimated Gross Incomes: .*Gross Income.\d
        Gross Income per SqFt:   .*Income per SqFt.\d
        Estimated Expense:       .*Expense.\d
        Expense per Sqft:        Expense.*\d
+       Net:                     \AN.*.\d 
        Full Market Value:       .*Value.\d
        Market Value per Sqft:   ^M.*.*.*\d$
-       Net:                     (\AN.*.\d) 
        """ 
-
+   """ Patterns Works
+       Works in loop for all patterns
+       r0 = re.compile("Gross SqFt.\d")
+       gsqft = list(filter(r0.match, columns))
+       print("Gross Sqft:", gsqft)
+      """
+    
+   list_col = ["comp_avgstu", "comp_avggsft", "comp_avgginc", "comp_avggips", "comp_avgexp", 
+               "comp_avgeps", "comp_avgnet", "comp_avgfmv","comp_avgmvps"]
+   list_reg = ["Total Units.\d", "Gross SqFt.\d", ".*Gross Income.\d", ".*Income per SqFt.\d", ".*Expense.\d",
+               "Expense.*\d", "\AN.*.\d", ".*Value.\d", "^M.*.*.*\d$"]
+   'This loop will grab the data then group by the columns as an average' 
+   y = 0
+   while(y != 9):
+       'Creates the regex that will help find the columns that match the pattern'
+       'col_n is the list of all the values that fit the pattern'
+       r = re.compile(list_reg[y])
+       col_n = list(filter(r.match, columns))
+       
+       'This collapse the columns that are in col_n and averages them'
+       x = data[col_n].aggregate(["mean"], axis = 1, skipna = False).round()    
+       print(x)
+       
+       'Makes a new column and makes it part of the new data object'
+       new_col = list_col[y]
+       data[new_col] = x
+       
+       if(y == (1|2|4|6|7)):
+           data[new_col] = data[new_col].div(10**6).round(2)
+       
+       y+=1
+   
+   list(data.columns) 
+   print(data)
+   return data
+   
 'This function works!!!' 
 'This list out all the columns names with key values so if I want to access these values individually'
 'I can do that and I can also make this into a dataframe'                       
-'clean_data = pd.DataFrame(pd.Series(filter_out(data))).T'
 clean_data = filter_out(data)
 clean_data = pd.DataFrame(clean_data.items(), columns=['Key', 'Name of Column'])
 columns = clean_data['Name of Column'].values.tolist()
-'This creates a series out of the Name of Column variable in the clean_data object'
-columns = pd.Series(clean_data['Name of Column'])
-print(clean_data)
 
-
-'Both of these work, it will get complicated to '
-columns[columns.str.match(r'(^G.*)') == True]
-'Has gross income per sqft and gross sqft for just comparables'
-gross = columns[columns.str.match(r'(\AG.*.\d)') == True]
-'Has Full Market Values for just comparables'
-full_market = columns[columns.str.match(r'(\AF.*.\d)') == True]
-'Has estimated gross income, estimated expense and expense per sqft for just comparables'
-estimated = columns[columns.str.match(r'(\AE.*.\d)') == True]
-'Has net incomes for the comparables'
-net = columns[columns.str.match(r'(\AN.*.\d)') == True]
-
-print(net)
-print(gross)
-print(full_market)
-print(estimated)
-
-regextolist(columns)
 
 'Make a function that wrangles the columns that have numeric values and groups or aggregates by neighborhood'
+data1 = regextolist(data1, columns)
+
+'Eliminates columns that have no number values'
+data1.drop(["Boro-Block-Lot", "Borough_ID", "Borough"], axis = 1) 
+
+'Eliminates any remaining columns that have numbered values'
+data1 = data1.drop(data1.filter(regex = '\d$'), axis = 1)    
+
+'Groups the data by Neighborhood which are averages for each column'
+data1 = data1.groupby('Neighborhood').aggregate(["mean"], skipna = False).round(2)
+data1.columns.droplevel(1)
+
+data1.to_excel("C:\\Users\\marvi\\OneDrive\\Documents\\GitHub\\Quant_R_Python\\Cleaned_Condo_Data.xlsx")
+
+'del clean_data, columns, data, data1'
+
--- condo_analysis.R
+++ condo_analysis.R
@@ -12,29 +12,9 @@ as.data.frame(initial_data)
 colnames(initial_data)
 str(initial_data)
 
-#principles for dealing with the changing world order
-#Make use of the factor function to categorize the neighborhood data 
-#Categorize the data into zip code info, hence making the column numerical
-#Then make a regression off with Income, Expenditures as the regressors
-#Then I will use Gross Sqft(greater than some number) and Neighborhood as a dummy variable
-factor_neighborhood <- factor(Neighborhood, levels = rev(unique(Neighborhood)), ordered = F)
-levels(factor_neighborhood)
-
-help(collapse)
-  
-#Here I will create dummy variables
-
-#I make use of the ifelse function to create the dummy variables
-#Based off of the code in the Boro-Block-Lot where the first number would determine
-#what Borough the property is located in New York
-initial_data$In_Man <- ifelse(grepl("1-", initial_data$`Boro-Block-Lot`), 1, 0)
-initial_data$In_Bx <- ifelse(grepl("2-", initial_data$`Boro-Block-Lot`), 1, 0)
-initial_data$In_Bk <- ifelse(grepl("3-", initial_data$`Boro-Block-Lot`), 1, 0)
-initial_data$In_Qn <- ifelse(grepl("4-", initial_data$`Boro-Block-Lot`), 1, 0)
-initial_data$In_SI <- ifelse(grepl("5-", initial_data$`Boro-Block-Lot`), 1, 0)
-
 initial_data <- initial_data %>% select(-contains("Condo")) 
-initial_data<- initial_data %>% select(-contains("Address"))
+initial_data <- initial_data %>% select(-contains("Address"))
+initial_data <- initial_data %>% select(-contains("Year"))
 complete_data<- initial_data %>% select(-contains("Building"))
 
 colnames(complete_data)
@@ -43,53 +23,35 @@ colnames(complete_data)
 write_xlsx(complete_data,
            "C:\\Users\\marvi\\OneDrive\\Documents\\GitHub\\Quant_R_Python\\Cleaned_Condo_Data.xlsx")
 
+#After cleaning
+c_data <- read_excel("C:\\Users\\marvi\\OneDrive\\Documents\\GitHub\\Quant_R_Python\\Cleaned_Condo_Data.xlsx")
+attach(c_data)
+as.data.frame(c_data)
+
+
 #If I need more dummy variables I'll use the summary stats for these two variables
-summary(`Total Units`)
-summary(`Gross SqFt`)
+summary(c_data$`Total Units`)
+summary(c_data$`Gross SqFt`)
 
 #Based off of the summary statistics above I'll use the medians to determine
 #What the dummy variables will be based on.
-complete_dataset$Units_more_than_71 <- ifelse(`Total Units` > 71.0, 1, 0)
-complete_dataset$GSqft_more_than_34k<- ifelse(`Gross SqFt` > 35990, 1, 0)
+c_data$Units_more_than_30 <- ifelse(`Total Units` > 30.0, 1, 0)
+c_data$GSqft_more_than_58k<- ifelse(`Gross SqFt` > 58000, 1, 0)
 
-
-#now I'm checking the median and max of gross sqft and total units, this is for each neighborhood
-tapply(complete_data$`Gross SqFt`, complete_data$Neighborhood, mean)
-tapply(complete_data$`Gross SqFt`, complete_data$Neighborhood, median)
-tapply(complete_data$`Total Units`, complete_data$Neighborhood, max)
-tapply(complete_data$`Total Units`, complete_data$Neighborhood, median)
-
-
-
-colnames(complete_data)
+colnames(c_data)
 
 #Correlates Year Built, Gross Sqft, Estimated Gross Income, Estimated Expense Net and Full Market Value
 #Update the columns are different so this wrong and will need to be corrected
-cor(initial_data[, c(7:9, 11, 13, 14)], use = "complete.obs")
-
-#Correlates Full Market Values of the other comparable apartments
-#Update the columns are different so this wrong and will need to be corrected
-cor(initial_data[, c(13, 14, 27, 42, 57)], use = "complete.obs")
+cor(c_data[, c(2:19)], use = "complete.obs")
 
 #linear regression function on full market value, with the regressors
 #size or Gross.sqft and Net.operating.income
-summary(lm(`Full Market Value` ~ `Net Operating Income` + `Net Operating Income 1` + `Net Operating Income 2`, initial_data))
+summary(lm(`Full Market Value` ~ comp_avgnet + comp_avggsft, c_data))
+pairs(~ `Full Market Value` + comp_avggsft + comp_avgnet + comp_avggips)
 
-summary(lm(`Full Market Value` ~ `Net Operating Income 1` + `Net Operating Income 2`
-           + `Net Operating Income 3`,   complete_dataset))
+summary(lm(`Full Market Value` ~ comp_avgnet + comp_avggips, c_data))
 #Adjusts the plot print 
-par(mfrow = c(2, 2))
-plot(lm(`Full Market Value` ~ `Net Operating Income 1` + `Net Operating Income 2`
-        + `Net Operating Income 3`,   complete_dataset))
 par(mfrow = c(1, 1))
 plot(lm(`Full Market Value` ~ `Estimated Expense 1` + `Estimated Expense 2` + `Estimated Expense 3`, complete_dataset))
-#Do this 
-pairs(~ `Full Market Value` + `Net Operating Income 1` + `Net Operating Income 2`)
-#Find the mean of the three comparable properties for all numeric data
-#Also make a dummy for the Boro-Block-Lot, first look for the first index and
-#if it is 1 then make a column called borough and determine the borough name based 
-#on the first index of Boro-Block-Lot
 
-
-
-rm(list = ls())
+rm(data)

7739d183e0a9d8b882958c6eddb4a59b7d36b443
mh48642n <mh48642n@pace.edu>
2023-06-21 15:01:10 -0400

    Near completed Graph

--- Time Analysis/Time_Series_Building.R
+++ Time Analysis/Time_Series_Building.R
@@ -24,36 +24,45 @@ fonts()
 
 #Plots the euro rates to dates with a normal ggplot
 euro_rates <- ggplot(data, mapping = aes(date, rate)) + 
-   geom_line(linewidth = 0.05, color = "orange") + 
-   theme_economist() +
-   theme(axis.title.x = element_blank(),
-         axis.text.x = element_text(size = 9.5, angle = 45, vjust = 0.7),
-         axis.title.y = element_text(size = 12,hjust = 0.5, vjust = 0),
-         axis.text.y = element_text(size = 9.5, vjust = 0.7),
-         plot.title = element_text(face = "bold", color = "white", size = 15),
-         plot.subtitle = element_text(size = 10, color = "black", hjust = 0), 
-         axis.title = element_text()) +
-   scale_x_date(breaks = "1 year", labels = date_format("%m/%Y")) 
+   geom_line(linewidth = 0.35, color = "lightblue") + 
+   theme_economist_white() + labs(y = "US Dollars to One Euro") +
+   theme(axis.title.y = element_text(family = "Georgia",
+                                     face = "bold",
+                                     colour = "white",
+                                     size = 10),
+         axis.title.x = element_blank(),
+         axis.text.x = element_text(size = 7.5, angle = 45, vjust = 0.7),
+         axis.text.y = element_text(size = 7.5)) +
+   scale_x_date(breaks = "1 year", labels = date_format("%m/%Y"))
 euro_rates
   
 #Makes the graph interactive so you can trace mouse over plot line
 #Added a range slider to find accurate dates
-#fix this font changes so shift font stuff to plotly function, subtitle and caption are gone as well
-fig <- ggplotly(euro_rates) %>% 
-    add_trace(x = ~date, 
-              y = ~rate 
-              )%>% 
-    layout(showlegend = F, 
-           xaxis = list(rangeslider = list(visible = T),
-           title = list(color = "white",
-                        text = "Exchange Rates: US Dollars to Euro",
-                        family = "Goudy Old Style"
-                        ))
-           )   
-fig
+titlefont <-list(family = "Georgia",
+                 size = 17,
+                 color = "orange")
+label <- list(bgcolor = "tan",
+              bordercolor = "transparent",
+              font = list(family = "Georgia",
+                          size = 11,
+                          color = "white"))
 
+fig <- ggplotly(euro_rates, type = 'scatter', mode = 'line') %>% 
+    add_trace(x = ~date, 
+              y = ~rate
+              )%>% 
+    layout(title = list(text = "Exchange Rates: US Dollars to Euro",
+                        font = titlefont),
+           xaxis = list(rangeslider = list(visible = T),
+           yaxis = list(fixedrange = TRUE)
+           ))%>%
+    style(hoverlabel = label)
+fig
+    
+ 
+#Saves the plot as a html file
 library(htmlwidgets)
-saveWidget(fig, "Euro_rates.html")
+saveWidget(fig, "Euro_rates_graph.html")
 
 
 rm(list = ls())

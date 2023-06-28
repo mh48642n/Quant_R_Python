18c310fa3da672f5fd024b32b6fc6d7687ccfa1d
mh48642n <mh48642n@pace.edu>
2023-06-28 12:35:06 -0400

    Changes to graph and new dataframe

--- Time Analysis/EU_rates_analysis.py
+++ Time Analysis/EU_rates_analysis.py
@@ -5,31 +5,40 @@ Created on Mon Jun 12 22:32:19 2023
 @author: marvi
 """
 """
-Pulling data from columns A12:A6376 and B12:B6376
+Pulling data from columns (A:D)3:(A:D)6371 
 
 """
 
 import openpyxl as op
 import pandas as pd
+import numpy as np
 
 
-#pulls data and makes a specfic worksheet active , allowing us to to interact
-sheet = op.load_workbook("C:\\Users\\marvi\\OneDrive\\Documents\\GitHub\\Quant_R_Python\\Time Analysis\\Euro_Exchange_Rates.xlsx")
+#pulls data and makes a specfic worksheet active, allowing us to to interact
+sheet = op.load_workbook("C:\\Users\\marvi\\OneDrive\\Documents\\GitHub\\Quant_R_Python\\Time Analysis\\Incubator_Project_-_Daily_Exchange_rates_1999_to_now.xlsx")
+sheet.active = sheet["Conversion Rates"]
 sheet = sheet.active
 
-#iterates through column A from cell 12 to 6376 accounting for dates
-dates = [sheet.cell(row=i,column=1).value for i in range(12,6376)]
-print(dates[:10])
 
-#iterates through column B from cell 12 to 6376 accounting for rates 
-rates = [sheet.cell(row=i,column=2).value for i in range(12,6376)]
-print(rates[:10])
+#iterates through column A from cell 12 to 6376 accounting for dates
+dates = [sheet.cell(row=i,column=1).value for i in range(3,6371)]
+
+#iterates through column B-D from cell 3 to 6371 accounting for rates 
+DEXSEU_Euro = [sheet.cell(row=i,column=2).value for i in range(3,6371)]
+DEXJPUS_Yen = [sheet.cell(row=i,column=3).value for i in range(3,6371)]
+DEXCHUS_Yuan = [sheet.cell(row=i,column=4).value for i in range(3,6371)]
+    
 
 #creates dictionary out of the lists and then the dictionary is converted to df
-dictt = {"date": dates, "rate": rates}
+dictt = {"dates": dates, "Euro": DEXSEU_Euro, "Yen": DEXJPUS_Yen, "Yuan": DEXCHUS_Yuan}
 r_to_d = pd.DataFrame(dictt)
 
-r_to_d.to_excel('Exchange_numbers_.xlsx', sheet_name = "Euro exchange rates", index = False)
+#Replaces all values called Not Available with an actual Nan value to make
+#analysis in R a bit easier
+r_to_d.replace(to_replace = ["Not Available"], value = np.nan, inplace = True)
+
+
+r_to_d.to_excel('Rates_and_dates.xlsx', sheet_name = "Conversion Rates", index = False)
 
 
 

da48bfd3e817e46b4c9cdc03e46fa0c1a7f06db6
mh48642n <mh48642n@pace.edu>
2023-07-22 16:28:15 -0400

    Currently adding more lines to graph

---
+++ Time Analysis/Data_Wrangling.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 12 22:32:19 2023
+
+@author: marvi
+"""
+"""
+Pulling data from columns (A:D)3:(A:D)6371 
+
+"""
+
+import openpyxl as op
+import pandas as pd
+import numpy as np
+
+
+#pulls data and makes a specfic worksheet active, allowing us to to interact
+bk = op.load_workbook("C:\\Users\\marvi\\OneDrive\\Documents\\GitHub\\Quant_R_Python\\Time Analysis\\Incubator_Project_-_Daily_Exchange_rates_1999_to_now.xlsx") 
+bk.active = bk["Conversion Rates"]
+sheet = bk.active
+
+#iterates through column A from cell 12 to 6376 accounting for dates
+dates = [sheet.cell(row=i,column=1).value for i in range(3,6371)]
+
+#iterates through column B-D from cell 3 to 6371 accounting for rates 
+DEXSEU_Euro = [sheet.cell(row=i,column=2).value for i in range(3,6371)]
+DEXJPUS_Yen = [sheet.cell(row=i,column=3).value for i in range(3,6371)]
+DEXCHUS_Yuan = [sheet.cell(row=i,column=4).value for i in range(3,6371)]
+    
+
+#creates dictionary out of the lists and then the dictionary is converted to df
+dictt = {"dates": dates, "Euro": DEXSEU_Euro, "Yen": DEXJPUS_Yen, "Yuan": DEXCHUS_Yuan}
+r_to_d = pd.DataFrame(dictt)
+
+#Close this workbook
+bk.close()
+
+#Replaces all values called Not Available with an actual Nan value to make
+#analysis in R a bit easier
+r_to_d.replace(to_replace = ["Not Available"], value = np.nan, inplace = True)
+
+
+r_to_d.to_excel('Rates_and_dates.xlsx', sheet_name = "Conversion Rates", index = False)
+
+
+#adding the 10-year maturities minus 2-year maturities and 
+#10 year maturity minus the 3 month maturities 
+#then saving to Rates_and_dates.xlsx
+
+def newsheet(data1, data2):
+    wk1 = op.load_workbook(data1)
+    wk2 = op.load_workbook(data2)
+    sheet1 = wk1.active
+    sheet2 = wk2.active
+    
+    dates = [sheet1.cell(row = i, column = 1).value for i in range(12, 6379)]
+    yields_3m = [sheet1.cell(row = i, column = 2).value for i in range(12, 6379)] 
+    yields_2y = [sheet2.cell(row = i, column = 2).value for i in range(12, 6379)] 
+   
+    
+    dictt = {"Dates": dates, 
+             "3-mth yields" : yields_3m, 
+             "Name for 2-yr yields" : yields_2y}
+    
+    yields_to_d = pd.DataFrame(dictt)
+    return yields_to_d
+
+#Getting new dataframe from the newsheet function
+ph1 = "C:\\Users\marvi\Downloads\T10Y3M.xlsx"
+ph2 = "C:\\Users\marvi\Downloads\T10Y2Y.xlsx"
+data = newsheet(ph1, ph2)
+
+
+#There are some NA values but those are in fact 0
+#So I'll use the replace function
+data.replace(to_replace = ["=NA()"], value = 0, inplace = True)
+
+
+#Before adding new dataframe to excel worlbook first create the sheet
+#Load the workbook, create the sheet with the create_sheet func and then save the path
+data.to_excel("Yields.xlsx", sheet_name = "Yield Rates", index = False)
+
+
+del data, lk, page, ph1, ph2, rate_sheet, tk, yield_sheet 
+    
+
+    
+    
+    
--- Time Analysis/Time_Series_Building.R
+++ Time Analysis/Time_Series_Building.R
@@ -3,27 +3,32 @@ setwd("C:\\Users\\marvi\\OneDrive\\Documents\\R")
 
 #Finds data, attaches the data, and determines datatypes of data
 library(readxl)
-data <- read_excel(file.choose())
-attach(data)
-str(data)
+rates <- read_excel(file.choose())
+yield <- read_excel(file.choose())
+attach(rates)
+str(rates)
+
+#Here we are merging the two dataframes together to create one dataframe
+#Also we are making this a excel file to make it more accesible for next time
+library(writexl)
+data <- merge(rates, yield, by = "dates")
+write_xlsx(data, "C:\\Users\\marvi\\Onedrive\\Documents\\Dataset_yr&d.xlsx")
 
 #Turns datatype of column date from POSIXct to
 library(dplyr)
+library(scales)
 data <- data %>% 
-  mutate(date = as.Date(date, format = "%Y-%m-%d"))
+  mutate(dates = as.Date(dates, format = "%Y-%m-%d"))
 
 library(ggplot2)
 library(plotly)
-library(scales)
 library(ggthemes)
 library(extrafont)
 #allYourFigureAreBelongToUs ggthemes for tha themes
-
-font_import()
 fonts()
-
 #Plots the euro rates to dates with a normal ggplot
-euro_rates <- ggplot(data, mapping = aes(date, rate)) + 
+#swap out rate for column name for Euro, Yuan or Yen and it should work
+euro_rates <- ggplot(data, mapping = aes(date, euro)) + 
    geom_line(linewidth = 0.35, color = "orange") + 
    theme_economist_white() + labs(y = "US Dollars to One Euro\n") +
    theme(axis.title.y = element_text(family = "Georgia",
@@ -46,7 +51,10 @@ euro_rates
 #fonts for the title and tooltip label
 titlefont <-list(family = "Georgia",
                  size = 20,
-                 color = "lightblue")
+                 color = "black")
+axesfont <- list(family = "Georgia",
+                 size = 16,
+                 color = "black")
 label <- list(bgcolor = "orange",
               bordercolor = "transparent",
               font = list(family = "Georgia",
@@ -63,15 +71,84 @@ fig <- ggplotly(euro_rates, type = 'scatter', mode = 'line') %>%
                         font =titlefont,
                         x = 0.0446,
                         y = 10.5),
-           xaxis = list(rangeslider = list(visible = T),
+           xaxis = list(rangeslider = list(visible = T)),
            yaxis = list(fixedrange = FALSE)
-           ))%>%
+           )%>%
     style(hoverlabel = label)
 fig  
- 
 #Saves the plot as a html file
 library(htmlwidgets)
 saveWidget(fig, "euro_rates_graph.html")
+
+#Now we'll make an interactive graph with four rates at once
+
+#Develops the buttons
+#This right here WORKSSSS
+B_Euro <- list(
+      label = "Euro",
+      method = "update",
+      args = list(list(visible = c(TRUE, FALSE, FALSE, TRUE)))
+      )
+B_Yuan <- list(
+      label = "Yuan",
+      method = "update",
+      args = list(list(visible = c(FALSE, TRUE, FALSE, TRUE)))
+    )
+B_Yen  <- list(
+      label = "Yen",
+      method = "update",
+      args = list(list(visible = c(FALSE, FALSE, TRUE, TRUE)))
+      )
+B_All <- list(
+    label = "All",
+    method = "update",
+    args = list(list(visible = c(TRUE, TRUE, TRUE, TRUE)))
+    )
+#Making other axes to make graph readable
+
+#Plot with three lines: Euro, Yuan, and Yen  
+viz1 <- plot_ly(data, x = ~dates,y = ~Euro, name = 'Euro', type = "scatter", mode = "lines", 
+                line = list(color = 'rgb(255, 128, 0)'))%>% 
+                add_trace(y = ~Yuan, name = 'Yuan', mode = "lines", 
+                          type = "scatter", line = list(color = 'rgb(0, 128, 255)'))%>%
+                add_trace(y = ~Yen, name = 'Yen', mode = "lines", 
+                          type = "scatter", line = list(color = 'rgb(255, 51, 51)'))
+#Buttons kind of work!!!
+#Rangeslider is operating hella well!!!
+#Hovermode = "x unified" helps to make the data readable for all three lines
+#good up to this point, fix merge problem and that's it
+viz2 <- viz1 %>% layout(plot_bgcolor = 'white',
+                        paper_bgcolor = 'lightgray',  
+                 updatemenus = list(
+                        list(type = 'buttons',
+                        direction = "up",
+                        xanchor = 'right',
+                        yanchor = 'top',
+                        x = 1.25, y = 1, 
+                        showactive = FALSE,
+                        buttons = list(B_Euro, B_Yuan, B_Yen, B_All))
+                        ),
+                  xaxis = list(title = "Dates", 
+                               rangeslider = list(visible = T)),
+                  yaxis = list(title = "Spot Exchange Rates"),
+                  hovermode = "x unified",
+                  legend = list(orientation = "v", x = 100, y = 0.90,
+                                title=list(text='<b> Legend </b>'))
+                 )
+viz2
+#Adding a 2 plots with 4 lines so adding the yields to the 3 currency rates
+yield_2y <- viz2 %>%  add_trace(data, x = ~dates, y = ~data$, yaxis2 = "y2",
+                                name = "2yr Yields", mode = "lines",
+                                line = list(color = 'rgb(2, 251, 115)'))
+yield_2y <- yield_2y %>% layout(title = list(text ="Exchange Rates and 2yr Yields",
+                                             font = titlefont),
+                                yaxis2 = list(overlaying = "y",
+                                              side = "right",
+                                              title = "2yr Yields"
+                                              )
+                                )
+library(htmlwidgets)
+saveWidget(viz2, "Yields_Rates_&Dates.html")
 
 
 rm(list = ls())

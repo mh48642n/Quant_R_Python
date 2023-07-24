ef7b25b23515565f3b1399fd8640d380a74a6c76
mh48642n <mh48642n@pace.edu>
2023-07-24 01:54:10 -0400

    Changes and new files

--- Time Analysis/Time_Series_Building.R
+++ Time Analysis/Time_Series_Building.R
@@ -1,11 +1,11 @@
 #Sets working directory
-setwd("C:\\Users\\marvi\\OneDrive\\Documents\\R")
+setwd("C:\\Users\\marvi\\OneDrive\\Documents")
 
 #Finds data, attaches the data, and determines datatypes of data
 library(readxl)
 rates <- read_excel(file.choose())
 yield <- read_excel(file.choose())
-attach(rates)
+attach(rates)#and yields
 str(rates)
 
 #Here we are merging the two dataframes together to create one dataframe
@@ -13,8 +13,10 @@ str(rates)
 library(writexl)
 data <- merge(rates, yield, by = "dates")
 write_xlsx(data, "C:\\Users\\marvi\\Onedrive\\Documents\\Dataset_yr&d.xlsx")
+data <- read_excel(file.choose())
 
-#Turns datatype of column date from POSIXct to
+#Turns datatype of column date from POSIXct to custom
+#write this to the excel sheet
 library(dplyr)
 library(scales)
 data <- data %>% 
@@ -80,24 +82,32 @@ fig
 library(htmlwidgets)
 saveWidget(fig, "euro_rates_graph.html")
 
-#Now we'll make an interactive graph with four rates at once
+#Now we'll make an interactive graph with three rates at once
+viz1 <- plot_ly(data, x = ~dates,y = ~Euro, name = 'Euro', type = "scatter", mode = "lines", 
+                line = list(color = 'rgb(255, 128, 0)'))%>% 
+  add_trace(y = ~Yuan, name = 'Yuan', mode = "lines", 
+            type = "scatter", line = list(color = 'rgb(0, 128, 255)'))%>%
+  add_trace(y = ~Yen, name = 'Yen', mode = "lines", 
+            type = "scatter", line = list(color = 'rgb(255, 51, 51)'))
+
+#Now we'll add the 10 year Treasury minus 2 year treasury yield curve with buttons
 
 #Develops the buttons
 #This right here WORKSSSS
 B_Euro <- list(
       label = "Euro",
       method = "update",
-      args = list(list(visible = c(TRUE, FALSE, FALSE, TRUE)))
+      args = list(list(visible = c(TRUE, TRUE, FALSE, FALSE)))
       )
 B_Yuan <- list(
       label = "Yuan",
       method = "update",
-      args = list(list(visible = c(FALSE, TRUE, FALSE, TRUE)))
+      args = list(list(visible = c(FALSE, TRUE, TRUE, FALSE)))
     )
 B_Yen  <- list(
       label = "Yen",
       method = "update",
-      args = list(list(visible = c(FALSE, FALSE, TRUE, TRUE)))
+      args = list(list(visible = c(FALSE, TRUE, FALSE, TRUE)))
       )
 B_All <- list(
     label = "All",
@@ -105,50 +115,56 @@ B_All <- list(
     args = list(list(visible = c(TRUE, TRUE, TRUE, TRUE)))
     )
 #Making other axes to make graph readable
+y2 <- list(
+  side = "right",
+  anchor = "free",
+  position = 1,
+  title = "2yr Yields")
 
-#Plot with three lines: Euro, Yuan, and Yen  
-viz1 <- plot_ly(data, x = ~dates,y = ~Euro, name = 'Euro', type = "scatter", mode = "lines", 
-                line = list(color = 'rgb(255, 128, 0)'))%>% 
-                add_trace(y = ~Yuan, name = 'Yuan', mode = "lines", 
-                          type = "scatter", line = list(color = 'rgb(0, 128, 255)'))%>%
-                add_trace(y = ~Yen, name = 'Yen', mode = "lines", 
-                          type = "scatter", line = list(color = 'rgb(255, 51, 51)'))
-#Buttons kind of work!!!
-#Rangeslider is operating hella well!!!
+
+#Second Y axis is established!!!!
+viz <- plot_ly(data, x = ~dates, y = ~Euro, mode = "lines", type = "scatter",name = "Euro", line = list(color = "rgb(251,107,1)"))%>%
+  add_trace(x = ~dates, y = ~data$`2-yr yields`, mode = "lines", type = "scatter", yaxis = "y2", 
+            name = "2yr yields", line = list(color = 'rgb(251,194,2)'))%>%
+  layout(yaxis2 = list(overlaying = "y", side = "right", showgrid = F))
+
+#Added the other currencies
+viz <- viz %>% add_trace(y = ~Yuan, name = 'Yuan', mode = "lines", 
+                  type = "scatter", line = list(color = 'rgb(233,108,27)'))%>%
+               add_trace(y = ~Yen, name = 'Yen', mode = "lines", 
+                  type = "scatter", line = list(color = 'rgb(255, 51, 51)'))
+
+#Buttons work!!!
+#Rangeslider is operating !!!
 #Hovermode = "x unified" helps to make the data readable for all three lines
-#good up to this point, fix merge problem and that's it
-viz2 <- viz1 %>% layout(plot_bgcolor = 'white',
-                        paper_bgcolor = 'lightgray',  
-                 updatemenus = list(
-                        list(type = 'buttons',
-                        direction = "up",
-                        xanchor = 'right',
-                        yanchor = 'top',
-                        x = 1.25, y = 1, 
-                        showactive = FALSE,
-                        buttons = list(B_Euro, B_Yuan, B_Yen, B_All))
-                        ),
-                  xaxis = list(title = "Dates", 
-                               rangeslider = list(visible = T)),
-                  yaxis = list(title = "Spot Exchange Rates"),
-                  hovermode = "x unified",
-                  legend = list(orientation = "v", x = 100, y = 0.90,
-                                title=list(text='<b> Legend </b>'))
-                 )
-viz2
-#Adding a 2 plots with 4 lines so adding the yields to the 3 currency rates
-yield_2y <- viz2 %>%  add_trace(data, x = ~dates, y = ~data$, yaxis2 = "y2",
-                                name = "2yr Yields", mode = "lines",
-                                line = list(color = 'rgb(2, 251, 115)'))
-yield_2y <- yield_2y %>% layout(title = list(text ="Exchange Rates and 2yr Yields",
-                                             font = titlefont),
-                                yaxis2 = list(overlaying = "y",
-                                              side = "right",
-                                              title = "2yr Yields"
-                                              )
-                                )
-library(htmlwidgets)
-saveWidget(viz2, "Yields_Rates_&Dates.html")
+viz <- viz %>% layout(plot_bgcolor = 'white',
+                     paper_bgcolor = 'lightgray',  
+                     updatemenus = list(
+                       list(type = 'buttons',
+                            direction = "up",
+                            xanchor = 'right',
+                            yanchor = 'top',
+                            x = 1.25, y = 1, 
+                            showactive = FALSE,
+                            buttons = list(B_Euro, B_Yuan, B_Yen, B_All))
+                     ),
+                     title = list(text ="Exchange Rates and 2yr Yields",
+                                  font = titlefont),
+                     xaxis = list(title = list(text = "Dates",
+                                               font = axesfont),
+                                  rangeslider = list(visible = T)),
+                     yaxis = list(title = list(text = "Spot Exchange Rates",
+                                          font = axesfont)
+                                          ,showgrid = F),
+                     hovermode = "x unified",
+                     legend = list(orientation = "v", x = 100, y = 0.90,
+                                   title=list(text='<b> Legend </b>',
+                                              font = axesfont))
+                )
 
+library(htmlwidgets)
+saveWidget(viz, "twoYr_yields_&_currencies.html")
+
+#Now we'll build the graph with three currencies and 10 yr - 3 mth treasuries
 
 rm(list = ls())
